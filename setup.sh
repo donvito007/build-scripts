b17d0d797e52e4600764730e1a0584631c03f04e
霧雨三沢 <stevexian56@gmail.com>
2020-11-22 10:36:00 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-if [ $DRONE_COMMIT_BRANCH -eq raphael-q ];
-  then
-    git clone https://github.com/Laulan56/anykernel.git /drone/src/anykernel3 --branch raphael-q --depth 1
-else
-    git clone https://github.com/Laulan56/anykernel.git /drone/src/anykernel3 --branch miui-q --depth 1
-fi
+git clone https://github.com/Laulan56/anykernel.git /drone/src/anykernel3 --branch op_sm8250 --depth 1
