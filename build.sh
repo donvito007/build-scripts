caaa1503086b45e5808f6e1155e254f237489826
Stephen Sin <stevexian56@gmail.com>
2021-02-14 17:02:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -51,11 +51,13 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r353983e/bin
+export CLANG_PATH=/drone/linux-x86/clang-r353983e/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/drone/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/drone/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export KBUILD_BUILD_USER="b1c1"
+export KBUILD_BUILD_HOST="MarisaKernel"
 
 echo
 log "Set DEFCONFIG"
@@ -67,7 +69,7 @@ echo
 log "Build The Good Stuff"
 echo 
 
-make CC=clang O=out -j128
+make CC=clang O=out -j128 "LOCALVERSION=Marisa-${date}"
 
 mkzipb1c1
 
