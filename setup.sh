a3fa12e1a4710e99e51b12cd14dab34a14914cc9
Stephen Sin <stevexian56@gmail.com>
2021-02-15 20:38:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt update && apt-get install -y device-tree-compiler lz4c
+apt update && apt-get install -y device-tree-compiler liblz4-tool
 git clone https://github.com/Laulan56/anykernel.git /drone/src/anykernel3 --branch b1c1 --depth 1
