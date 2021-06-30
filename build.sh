897030e81cf9701cd374b1d1155ee2c7519b28fd
Stephen Sin <stevexian56@gmail.com>
2021-06-30 15:27:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ gcc_32_path="${HOME}/proton-clang/bin/arm-linux-gnueabi-"
 
 date="`date +"%m%d%H%M"`"
 firstver="Marisa"
-device1="kebab"
 middlever="RUBY"
 
 args="-j64 O=out \
@@ -42,76 +41,51 @@ terminate(){
 
 
 buildKebab(){
-	export KBUILD_BUILD_USER="Kebab"
+	export KBUILD_BUILD_USER="cepheus"
 	export KBUILD_BUILD_HOST="MarisaKernel"
 args+="LOCALVERSION=-${middlever}-${date} "
-	make $args kebab_defconfig&&make $args
+	make $args cepheus_defconfig&&make $args
 	if [ $? -ne 0 ]; then
-    terminate "Error while building for kebab!"
+    terminate "Error while building for cepheus!"
     fi
 	mkzipKebab
-	tg_notify "Finish building kebab!"
+	tg_notify "Finish building cepheus!"
 }
 
 mkzipKebab(){
-	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
-	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
+	mv -f ~/src/out/arch/arm64/boot/Image-dtb ~/src/anykernel3
 	cd ~/src/anykernel3
-	zip -r "MarisaKernel-kebab-$middlever-$date.zip" *
-	mv -f "MarisaKernel-kebab-$middlever-$date.zip" ${HOME}
+	zip -r "MarisaKernel-cepheus-$middlever-$date.zip" *
+	mv -f "MarisaKernel-cepheus-$middlever-$date.zip" ${HOME}
 	cd ${HOME}
 	log "Finish making zip for kebab!"
-	tg_upload "MarisaKernel-kebab-$middlever-$date.zip"
+	tg_upload "MarisaKernel-cepheus-$middlever-$date.zip"
 	cd $source
 }
 
 buildinstantnoodle(){
-	export KBUILD_BUILD_USER="instantnoodle"
+	export KBUILD_BUILD_USER="raphael"
 	export KBUILD_BUILD_HOST="MarisaKernel"
 args+="LOCALVERSION=-${middlever}-${date} "
-	make $args instantnoodle_defconfig&&make $args
+	make $args raphael_defconfig&&make $args
 	if [ $? -ne 0 ]; then
-    terminate "Error while building for instantnoodle!"
+    terminate "Error while building for raphael!"
     fi
 	mkzipinstantnoodle
-	tg_notify "Finish building instantnoodle!"
+	tg_notify "Finish building raphael!"
 }
 
 mkzipinstantnoodle(){
-	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
-	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
+	mv -f ~/src/out/arch/arm64/boot/Image-dtb ~/src/anykernel3
 	cd ~/src/anykernel3
-	zip -r "MarisaKernel-instantnoodle-$middlever-$date.zip" *
-	mv -f "MarisaKernel-instantnoodle-$middlever-$date.zip" ${HOME}
+	zip -r "MarisaKernel-raphael-$middlever-$date.zip" *
+	mv -f "MarisaKernel-raphael-$middlever-$date.zip" ${HOME}
 	cd ${HOME}
-	log "Finish making zip for instantnoodle!"
-	tg_upload "MarisaKernel-instantnoodle-$middlever-$date.zip"
+	log "Finish making zip for raphael!"
+	tg_upload "MarisaKernel-raphael-$middlever-$date.zip"
 	cd $source
 }
 
-buildinstantnoodlep(){
-	export KBUILD_BUILD_USER="instantnoodle"
-	export KBUILD_BUILD_HOST="MarisaKernel"
-args+="LOCALVERSION=-${middlever}-${date} "
-	make $args instantnoodlep_defconfig&&make $args
-	if [ $? -ne 0 ]; then
-    terminate "Error while building for instantnoodlep!"
-    fi
-	mkzipinstantnoodlep
-	tg_notify "Finish building instantnoodlep!"
-}
-
-mkzipinstantnoodlep(){
-	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
-	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
-	cd ~/src/anykernel3
-	zip -r "MarisaKernel-instantnoodlep-$middlever-$date.zip" *
-	mv -f "MarisaKernel-instantnoodlep-$middlever-$date.zip" ${HOME}
-	cd ${HOME}
-	log "Finish making zip for instantnoodlep!"
-	tg_upload "MarisaKernel-instantnoodlep-$middlever-$date.zip"
-	cd $source
-}
 tg_notify "LOG: START BUILDING!"
 cd /drone/src
 git submodule init
@@ -121,7 +95,5 @@ clean
 buildKebab
 cd /drone/src
 buildinstantnoodle
-cd /drone/src
-buildinstantnoodlep
 
 log "Build finished for #${DRONE_BUILD_NUMBER} ( ${date} )."
