891e79600b4e8740fb4d445a06e4851fc54975cb
Stephen Sin <stevexian56@gmail.com>
2022-04-16 21:50:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ gcc_32_path="${HOME}/proton-clang/bin/arm-linux-gnueabi-"
 
 date="`date +"%Y%m%d%H%M"`"
 firstver="Marisa"
-device1="kebab"
-middlever="RUBY"
+device="umi"
+middlever="r4"
 
-args="-j64 O=out \
+args="-j128 O=out \
 	ARCH=arm64 \
 	SUBARCH=arm64 "
 
@@ -41,141 +41,39 @@ terminate(){
 }
 
 
-buildKebab(){
-	export KBUILD_BUILD_USER="Kebab"
+build(){
+	export KBUILD_BUILD_USER="56"
 	export KBUILD_BUILD_HOST="MarisaKernel"
 args+="LOCALVERSION=-${middlever}-${date} "
-	make $args kebab_defconfig&&make $args
+	make $args ${device}_defconfig&&make $args
 	if [ $? -ne 0 ]; then
-    terminate "Error while building for kebab!"
+    terminate "Error while building for ${device}!"
     fi
-	mkzipKebab
-	tg_notify "Finish building kebab!"
+	mkzip
+	tg_notify "Finish building ${device}!"
 }
 
-mkzipKebab(){
+mkzip(){
 	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
+	mv -f ~/src/out/arch/arm64/boot/dtbo.img ~/src/anykernel3
 	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
 	cd ~/src/anykernel3
-	zip -r "MarisaKernel-kebab-$middlever-$date.zip" *
-	mv -f "MarisaKernel-kebab-$middlever-$date.zip" ${HOME}
+	zip -r "MarisaKernel-${device}-$middlever-$date.zip" *
+	mv -f "MarisaKernel-${device}-$middlever-$date.zip" ${HOME}
 	cd ${HOME}
-	log "Finish making zip for kebab!"
-	tg_upload "MarisaKernel-kebab-$middlever-$date.zip"
+	log "Finish making zip for ${device}!"
+	tg_upload "MarisaKernel-${device}-$middlever-$date.zip"
 	cd $source
 }
 
-buildKebabaosp(){
-	export KBUILD_BUILD_USER="Kebab"
-	export KBUILD_BUILD_HOST="MarisaKernel"
-args+="LOCALVERSION=-${middlever}-${date} "
-	make $args kebab_defconfig&&make $args
-	if [ $? -ne 0 ]; then
-    terminate "Error while building for kebab AOSP!"
-    fi
-	mkzipKebabaosp
-	tg_notify "Finish building kebab AOSP!"
-}
-
-mkzipKebabaosp(){
-	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
-	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
-	cd ~/src/anykernel3
-	zip -r "MarisaKernel-kebab-$middlever-$date-AOSP.zip" *
-	mv -f "MarisaKernel-kebab-$middlever-$date-AOSP.zip" ${HOME}
-	cd ${HOME}
-	log "Finish making zip for kebab AOSP!"
-	tg_upload "MarisaKernel-kebab-$middlever-$date-AOSP.zip"
-	cd $source
-}
-
-buildinstantnoodleaosp(){
-	export KBUILD_BUILD_USER="instantnoodle"
-	export KBUILD_BUILD_HOST="MarisaKernel"
-args+="LOCALVERSION=-${middlever}-${date} "
-	make $args instantnoodle_defconfig&&make $args
-	if [ $? -ne 0 ]; then
-    terminate "Error while building for instantnoodle AOSP!"
-    fi
-	mkzipKebabaosp
-	tg_notify "Finish building instantnoodle AOSP!"
-}
-
-mkzipinstantnoodleaosp(){
-	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
-	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
-	cd ~/src/anykernel3
-	zip -r "MarisaKernel-instantnoodle-$middlever-$date-AOSP.zip" *
-	mv -f "MarisaKernel-instantnoodle-$middlever-$date-AOSP.zip" ${HOME}
-	cd ${HOME}
-	log "Finish making zip for instantnoodle AOSP!"
-	tg_upload "MarisaKernel-instantnoodle-$middlever-$date-AOSP.zip"
-	cd $source
-}
-
-
-buildinstantnoodle(){
-	export KBUILD_BUILD_USER="instantnoodle"
-	export KBUILD_BUILD_HOST="MarisaKernel"
-args+="LOCALVERSION=-${middlever}-${date} "
-	make $args instantnoodle_defconfig&&make $args
-	if [ $? -ne 0 ]; then
-    terminate "Error while building for instantnoodle!"
-    fi
-	mkzipinstantnoodle
-	tg_notify "Finish building instantnoodle!"
-}
-
-mkzipinstantnoodle(){
-	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
-	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
-	cd ~/src/anykernel3
-	zip -r "MarisaKernel-instantnoodle-$middlever-$date.zip" *
-	mv -f "MarisaKernel-instantnoodle-$middlever-$date.zip" ${HOME}
-	cd ${HOME}
-	log "Finish making zip for instantnoodle!"
-	tg_upload "MarisaKernel-instantnoodle-$middlever-$date.zip"
-	cd $source
-}
-
-buildinstantnoodlep(){
-	export KBUILD_BUILD_USER="instantnoodle"
-	export KBUILD_BUILD_HOST="MarisaKernel"
-args+="LOCALVERSION=-${middlever}-${date} "
-	make $args instantnoodlep_defconfig&&make $args
-	if [ $? -ne 0 ]; then
-    terminate "Error while building for instantnoodlep!"
-    fi
-	mkzipinstantnoodlep
-	tg_notify "Finish building instantnoodlep!"
-}
-
-mkzipinstantnoodlep(){
-	mv -f ~/src/out/arch/arm64/boot/Image.gz ~/src/anykernel3
-	mv -f ~/src/out/arch/arm64/boot/dts/vendor/qcom/kona-v2.1.dtb ~/src/anykernel3/dtb
-	cd ~/src/anykernel3
-	zip -r "MarisaKernel-instantnoodlep-$middlever-$date.zip" *
-	mv -f "MarisaKernel-instantnoodlep-$middlever-$date.zip" ${HOME}
-	cd ${HOME}
-	log "Finish making zip for instantnoodlep!"
-	tg_upload "MarisaKernel-instantnoodlep-$middlever-$date.zip"
-	cd $source
-}
 tg_notify "LOG: START BUILDING!"
 cd /drone/src
 git submodule init
 git submodule update
 
 clean
-buildKebab
+git reset --hard
+build
 cd /drone/src
-buildinstantnoodle
-cd /drone/src
-buildinstantnoodlep
-cd /drone/src
-git apply lineage.diff
-buildKebabaosp
-cd /drone/src
-buildinstantnoodleaosp
 
 log "Build finished for #${DRONE_BUILD_NUMBER} ( ${date} )."
