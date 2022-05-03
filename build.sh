7d7ad8afc8a22dc5c0fd03eea2b7da0bd79c16a4
Stephen Sin <stevexian56@gmail.com>
2022-05-03 20:39:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ args+="LOCALVERSION=-${middlever}-${date} "
 }
 
 mkzip1(){
-	mv -f ~/src/out/arch/arm64/boot/Image ~/src/anykernel3
+	mv -f ~/src/out/arch/arm64/boot/Image-dtb ~/src/anykernel3
 	cd ~/src/anykernel3
 	zip -r "MarisaKernel-${device}-$middlever-$date.zip" *
 	mv -f "MarisaKernel-${device}-$middlever-$date.zip" ${HOME}
